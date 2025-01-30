aef0412056e5bb055c0dd9122bf6682c79ed01f6
NyiKyawAye <nyikyaw0101@gmail.com>
2025-01-30 13:28:33 +0630

    Initial commit

---
+++ src/main/java/com/example/websocketnotifications/controller/NotificationController.java
@@ -0,0 +1,16 @@
+package com.example.websocketnotifications.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotificationController {
+
+    @MessageMapping("/send")
+    @SendTo("/topic/notifications")
+    public String sendNotification(String message) {
+        return message;
+    }
+}
+
---
+++ src/test/java/com/example/websocketnotifications/WebsocketNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
